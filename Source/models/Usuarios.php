0f0fbc41e84599bc1a2f451a0c047f52a2e7973f
adryanoalf <adryanoalf@gmail.com>
2017-09-24 20:47:32 -0300

    Documentação da classe Usuário

--- Source/models/Usuarios.php
+++ Source/models/Usuarios.php
@@ -1,5 +1,21 @@
 <?php
+/**
+ * This class retrieves and saves data of the user.
+ *
+ * @author  samuelrcosta
+ * @version 0.1.0, 09/18/2017
+ * @since   0.1
+ */
 class Usuarios extends model{
+
+    /**
+     * This function verify if the input is valid for any account registered.
+     * If valid returns it's ID, otherwise return -1 for false.
+     *
+     * @param   $email    The email registered for the account.
+     * @param   $senha    The current password.
+     * @return  A positive Integer for the user ID, or -1 for 'user not found'.
+     */
     public function logIn($email, $senha){
         $sql = "SELECT id FROM usuarios WHERE email = ? AND senha = ?";
         $sql = $this->db->prepare($sql);
@@ -13,11 +29,22 @@ class Usuarios extends model{
         }
     }
 
+    /**
+     * Function used to unregister the user in the session.
+     *
+     * @return Always return -1.
+     */
     public function logOff(){
         unset($_SESSION['cLogin']);
         return -1;
     }
 
+    /**
+     * This function retrieves all data from an user, by using it's ID.
+     *
+     * @param   $id The user ID number saved in the database.
+     * @return  An array containing all data retrieved.
+     */
     public function getDados($id){
         $sql = "SELECT * FROM usuarios WHERE id = ?";
         $sql = $this->db->prepare($sql);
