cae4cf6c4a5cfde0fef5fe8fd32a80dc44e0f7ef
AdryanoALF <adryanoalf@gmail.com>
2017-10-18 23:31:41 -0200

    Correção de erro de junção de código
    Foi enviado as duas versões juntas por engano do kraken.

--- Source/models/Clientes.php
+++ Source/models/Clientes.php
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-<?php
-/**
- * This class retrieves and saves data of clients.
- *
- * @author  adryanoalf
- * @version 0.3.0, 18/06/2017
- * @since   0.3
- */
-class Clientes extends model {
-
-    /**
-     * This function retrieves all data from a client, by using it's ID.
-     *
-     * @param   $id The client ID saved in the database.
-     * @return  An array containing the data from the user.
-     */
-    public function getCliente($id) {
-        $sql = "SELECT * FROM clientes WHERE id = ?";
-        $sql = $this->db->prepare($sql);
-        $sql->execute(array($id));
-        $sql = $sql->fetch();
-        return $sql;
-    }
-
-    /**
-     * This function retrieves all data from all clients in database.
-     *
-     * @return  An array containing all data retrieved.
-     */
-    public function getClientes() {
-        $sql = "SELECT * FROM clientes WHERE status_interno = ?";
-        $sql = $this->db->prepare($sql);
-        $sql->execute(array(1));
-        $sql = $sql->fetchAll();
-        return $sql;
-    }
-
-}
-?>
-=======
 <?php
 /**
  * This class retrieves and saves data of the client.
@@ -130,4 +89,3 @@ class Clientes extends model{
         return $sql;
     }
 }
->>>>>>> master
